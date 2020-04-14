e451b7c51ecac5d1b692d1db3fd2620f07ec5fe8
gradie <mayombegradi@gmail.com>
2020-04-14 20:54:51 +0200

    Cards manipulations

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 This is my first elixir tutorial
+
+Important commands:
+* mix new project_name: A bit like rebar create-app appid=project_name in rebar.
+* iex -S mix: starts your interactive shell while running the mix script.
+* recompile/0: just recompiles everything inside your root project
---
+++ cards/lib/cards.ex
@@ -0,0 +1,27 @@
+defmodule Cards do
+  def hello do
+    "hi there!"
+  end
+
+  def create_deck do
+    values = ["Ace", "Two", "Three", "Four", "Five"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+
+    cards = for value <- values, suit <- suits do
+                "#{value} of #{suit}"
+            end
+    List.flatten(cards)
+  end
+
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
+  end
+
+  def deal(deck, hand_size) do
+    Enum.split(deck, hand_size)
+  end
+end
